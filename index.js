081103174bf9709fb766c900e074996dbeee0922
Andrew Smith <espadav8@gmail.com>
2017-05-24 11:22:30 +1000

    Whitespace

--- index.js
+++ index.js
@@ -10,21 +10,23 @@ module.exports = function createReleasePR (config) {
   return client
     .getNextReleaseVersion()
     .then(function (ver) {
-        version = ver
-        return client
-            .prepareReleaseBranch(version)
-            .then(function (releaseBranchRef) {
-                return client
-                    .prepareReleasePR(releaseBranchRef)
-                    .then(function (releasePR) {
-                        return client.collectReleasePRs(releasePR).then(function (prs) {
-                          var templatePath = config.template || __dirname + '/release.mustache'
-                          var template = fs.readFileSync(templatePath, 'utf8')
-                          var message = releaseMessage(version, template, prs)
+      version = ver
+      return client
+        .prepareReleaseBranch(version)
+        .then(function (releaseBranchRef) {
+          return client
+            .prepareReleasePR(releaseBranchRef)
+            .then(function (releasePR) {
+              return client
+                .collectReleasePRs(releasePR)
+                .then(function (prs) {
+                  const templatePath = config.template || __dirname + '/release.mustache'
+                  const template = fs.readFileSync(templatePath, 'utf8')
+                  const message = releaseMessage(version, template, prs)
 
-                          return client.updatePR(releasePR, message)
-                        })
-                    })
+                  return client.updatePR(releasePR, message)
+                })
             })
+        })
     })
 }
